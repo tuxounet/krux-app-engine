a87bd1f4b58b52923d949696568b4c6a877951d7
Christophe TIRAOUI <krux@krux.fr>
2022-09-11 20:50:22 +0000

    fix

--- src/components/KRequest.ts
+++ src/components/KRequest.ts
@@ -58,7 +58,7 @@ export class KRequest {
     return result;
   }
 
-  extractBody(fields: string[]): Record<string, string> | undefined {
+  extractBody(fields: string[]): Record<string, unknown> | undefined {
     if (!this.body) return undefined;
     if (!fields) return undefined;
     let result: Record<string, string> = {};
