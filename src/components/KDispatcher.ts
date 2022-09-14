f7f58048b5b199a6365a7ad9e8dd18c14ee19e10
Christophe TIRAOUI <krux@krux.fr>
2022-09-14 12:49:22 +0000

    fix dispatcher

--- src/components/KDispatcher.ts
+++ src/components/KDispatcher.ts
@@ -3,27 +3,29 @@ import { KRouter } from "./KRouter";
 export class KDispatcher {
   constructor(readonly router: KRouter) {}
 
-  emit(eventName: string, source: string, params?: Record<string, string>) {
+  emit(eventName: string, source: string, params?: Record<string, unknown>) {
     this.notify(eventName, source, params);
   }
 
-  private notify(eventName: string, source: string, params?: Record<string, string>) {
+  private notify(eventName: string, source: string, params?: Record<string, unknown>) {
     try {
-      Object.keys(this.router.connections).forEach((connectionId) => {
-        console.info("dispatcher", "notify", connectionId, eventName, source);
+      Object.keys(this.router.connections)
+        .filter((item) => this.router.connections[item] !== undefined)
+        .forEach((connectionId) => {
+          console.info("dispatcher", "notify", connectionId, eventName, source);
 
-        const socket = this.router.connections[connectionId];
-        if (socket) {
-          socket.send(
-            JSON.stringify({
-              kind: "event",
-              eventName,
-              source,
-              params,
-            })
-          );
-        }
-      });
+          const socket = this.router.connections[connectionId];
+          if (socket) {
+            socket.send(
+              JSON.stringify({
+                kind: "event",
+                eventName,
+                source,
+                params,
+              })
+            );
+          }
+        });
     } catch (e) {
       console.error("dispatcher", "notify", "failure", e);
     }
